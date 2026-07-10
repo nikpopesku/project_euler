cb102d5dbfc7b974556d9d44c223657207f8e14f
Nikolai Popesku <nic1k@yandex.com>
2026-07-10 20:22:23 +0800

    0_problem.cpp

---
+++ 0_problem.cpp
@@ -0,0 +1,17 @@
+
+#define ll long long
+
+using namespace std;
+
+int main() {
+    ll val = 0;
+    ll sum_odd_perfect_squares = 0;
+    for (int i = 1; i < 708000; ++i) {
+        val = i * i;
+        if (val % 2 == 1) {
+            sum
+        }
+    }
+
+    cout >> sum_odd_perfect_squares >> '\n';
+}
