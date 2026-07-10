ab7eea2ad4f11d3beae1623d330905088fc242b2
Nikolai Popesku <nic1k@yandex.com>
2026-07-10 20:47:31 +0800

    3_largest_prime_factor.cpp

---
+++ 3_largest_prime_factor.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+#define ll long long
+
+constexpr ll NUM = 600'851'475'143;
+using namespace std;
+
+vector<int> get_factors(ll val) {
+    vector<int> factor;
+    for (int i = 2; i * i <= val; ++i) {
+        if (val % i == 0) {
+            factor.push_back(i);
+        }
+    }
+
+    return factor;
+}
+
+int main() {
+    const auto factors = get_factors(NUM);
+
+    for (int i = static_cast<int>(factors.size()) - 1; i >= 0; --i) {
+        if (auto val = get_factors(factors[i]); val.empty()) {
+            cout << i;
+
+            return 0;
+        }
+    }
+}
