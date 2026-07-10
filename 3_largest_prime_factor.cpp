1e89dfb11a866eba6f1575a34a762e5c4766805a
Nikolai Popesku <nic1k@yandex.com>
2026-07-10 21:53:47 +0800

    4_largest_palindrome_product.cpp

--- 3_largest_prime_factor.cpp
+++ 3_largest_prime_factor.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <vector>
 
-#define ll long long
+#define LL long long
 
-constexpr ll NUM = 600'851'475'143;
+constexpr LL NUM = 600'851'475'143;
 using namespace std;
 
-vector<ll> get_factors(ll val) {
-    vector<ll> factor;
-    for (ll i = 2; i * i <= val; ++i) {
+vector<LL> get_factors(LL val) {
+    vector<LL> factor;
+    for (LL i = 2; i * i <= val; ++i) {
         if (val % i == 0) {
             factor.push_back(i);
         }
@@ -20,7 +20,7 @@ vector<ll> get_factors(ll val) {
 int main() {
     const auto factors = get_factors(NUM);
 
-    for (ll i = static_cast<int>(factors.size()) - 1; i >= 0; --i) {
+    for (LL i = static_cast<int>(factors.size()) - 1; i >= 0; --i) {
         auto val = get_factors(factors[i]);
 
         if (val.empty()) {
