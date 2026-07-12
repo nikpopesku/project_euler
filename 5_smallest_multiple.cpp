e2f952488f75d60585ab5e07efb0fa6ee3f263a9
Nikolai Popesku <nic1k@yandex.com>
2026-07-12 21:51:23 +0800

    5_smallest_multiple.cpp

--- 5_smallest_multiple.cpp
+++ 5_smallest_multiple.cpp
@@ -1,27 +1,18 @@
 #include <iostream>
 #include <vector>
 
-#define LL long long
+#define ll long long
 
 using namespace std;
 
 
 int main() {
-    int mx = -1;
-    for (int i = 999; i >= 111; --i) {
-        if (i * 999 <= mx) continue;
+    vector<ll> numbers = {2LL, 3LL, 2LL, 5LL, 7LL, 2LL, 3LL, 11LL, 13LL, 2LL, 17LL, 19LL};
+    ll smallest_multiple = 1;
 
-        for (int j = 999; j >= 111; --j) {
-            const int val = i * j;
-            if (val <= mx) continue;
-
-            string st = to_string(val);
-            string st2 = st;
-            reverse(st2.begin(), st2.end());
-
-            if (st == st2) mx = val;
-        }
+    for (int i = 0; i <= static_cast<int>(numbers.size()); ++i) {
+        smallest_multiple *= numbers[i];
     }
 
-    cout << mx;
+    cout << smallest_multiple;
 }
