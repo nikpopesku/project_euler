8f0aa54578820bdd3169a33215313d43c236ee5b
Nikolai Popesku <nic1k@yandex.com>
2026-07-25 18:15:59 +0800

    12_highly_divisible_triangle_number.cpp

---
+++ 12_highly_divisible_triangle_number.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#define ll long long
+
+constexpr int limit = 5;
+
+using namespace std;
+
+int main() {
+    int count_divisors = 0;
+    ll current_triangular_number = 0;
+    ll last_number = 0;
+    while (count_divisors <= limit) {
+        count_divisors = 0;
+        current_triangular_number += ++last_number;
+        for (ll i = 1; i <= current_triangular_number / 2; ++i) {
+            if (current_triangular_number % i == 0) ++count_divisors;
+        }
+        ++count_divisors;
+    }
+
+    cout << current_triangular_number << '\n';
+}
