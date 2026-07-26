62375a077fdba0d5a0abb430d34891f8cb562844
Nikolai Popesku <nic1k@yandex.com>
2026-07-26 17:40:19 +0800

    15_lattice_path.cpp

---
+++ 15_lattice_path.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <set>
+
+#define ll long long
+
+constexpr ll limit = 1'000'000;
+
+using namespace std;
+
+
+int main() {
+    ll answer = 1;
+    set<ll> st {};
+
+    for (ll i = 2; i <= 20; ++i) {
+        st.insert(static_cast<ll>(i));
+    }
+
+
+    for (ll i = 21; i <= 40; ++i) {
+        answer *= i;
+        vector<ll> erase {};
+        for (auto &denominator: st) {
+            if (answer % denominator == 0) {
+                answer /= denominator;
+                st.erase(denominator);
+                erase.push_back(denominator);
+            }
+        }
+    }
+
+    cout << "answer " << answer << '\n';
+}
