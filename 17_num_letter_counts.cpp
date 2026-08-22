1e0ed0488fb7339e2ab83939f3fc9e989783eb21
Nikolai Popesku <nic1k@yandex.com>
2026-08-22 21:20:46 +0800

    17_num_letter_counts.cpp

--- 17_num_letter_counts.cpp
+++ 17_num_letter_counts.cpp
@@ -1,9 +1,21 @@
-#include <cmath>
 #include <iostream>
 
 #define ll long long
 
 constexpr ll limit = 1'000;
+
+using namespace std;
+
+// Arrays to hold the spelling building blocks
+const vector<string> ONES = {
+    "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
+    "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", 
+    "Seventeen", "Eighteen", "Nineteen"
+};
+
+const vector<string> TENS = {
+    "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"
+};
 
 using namespace std;
 
