4eaf1f3cb0b904dbd3a8f911ff2c2d5291d116a3
Nikolai Popesku <nic1k@yandex.com>
2026-08-23 19:33:00 +0800

    17_num_letter_counts.cpp

--- 17_num_letter_counts.cpp
+++ 17_num_letter_counts.cpp
@@ -1,35 +1,36 @@
 #include <iostream>
-
-#define ll long long
+#include <vector>
 
 constexpr ll limit = 1'000;
 
 using namespace std;
 
-// Arrays to hold the spelling building blocks
-const vector<string> ONES = {
-    "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
-    "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", 
-    "Seventeen", "Eighteen", "Nineteen"
-};
-
-const vector<string> TENS = {
-    "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"
-};
 
 using namespace std;
 
 int main() {
     int cnt = 0;
 
+    // Arrays to hold the spelling building blocks
+    const vector<string> ones = {
+        "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
+        "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen",
+        "Seventeen", "Eighteen", "Nineteen"
+    };
+
+    const vector<string> tens = {
+        "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"
+    };
+
+
     for (int i = 1; i <= limit; ++i) {
         if (i >= 100) {
-            cnt += ONES[i / 100].size();
+            cnt += ones[i / 100].size();
             cnt += 10; //hundred and
         }
 
-        cnt += TENS[(i % 100) / 10].size();
-        cnt += ONES[i % 10].size();
+        cnt += tens[(i % 100) / 10].size();
+        cnt += ones[i % 10].size();
     }
 
     cout << cnt << '\n';
